ddc2e9d355e6051d460a77d2f9de5b30c047722b
shanwebfix <shanwebfix@gmail.com>
2025-08-04 18:09:52 +0600

    fetch script added

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,30 @@
+ 
+   function loadSection(path, elementId) {
+    fetch(path)
+      .then(res => res.text())
+      .then(html => {
+        document.getElementById(elementId).innerHTML = html;
+      })
+      .catch(err => {
+        document.getElementById(elementId).innerHTML = "কনটেন্ট লোড করতে সমস্যা হয়েছে!";
+        console.error(err);
+      });
+  }
+
+  loadSection("/keyboard-sortcut/key-sort.html", "key-section");
+  loadSection("/oparator/oparator.html", "sort-section");
+  loadSection("/islamic/islamic.html", "islamic-section");
+  loadSection("/daily-use-tools/daily-use.html", "daily-section");
+  loadSection("/dev-designer-tools/dev-design.html", "dev-section");
+  loadSection("/educational-tools/edu.html", "edu-section");
+  loadSection("/freelanching-tools/free.html", "free-section");
+  loadSection("/dokan-tools/dokan.html", "dokan-section");
+  loadSection("/money-tools/money.html", "money-section");
+  loadSection("/media-tools/media.html", "media-section");
+  loadSection("/bd-Localized-Special-tools/localized.html", "localized-section");
+  loadSection("/tools-for-general-people/general.html", "general-section");
+ 
+ 
  // io
   document.addEventListener("contextmenu", (e) => e.preventDefault());
 
@@ -300,3 +327,15 @@ document.getElementById("arabic-date").innerText = `${islDay} ${islMonth} ${islY
     rightArrow.addEventListener('click', () => {
       slider.scrollBy({ left: scrollAmount, behavior: 'smooth' });
     });
+
+
+
+
+
+
+
+
+    
+
+
+

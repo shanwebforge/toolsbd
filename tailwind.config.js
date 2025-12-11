34c933b389347d11ec9df4c8b3bd58780092a2d9
shanwebfix <shanwebfix@gmail.com>
2025-12-11 19:56:47 +0600

    tailwind update

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./**/*.html",     // root er shob HTML
+    "./src/**/*.html", // src folder er HTML
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
